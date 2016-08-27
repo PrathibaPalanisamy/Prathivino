8362441815eb63ea464388fba7b1ac9d30541c34
PrathibaPalanisamy <p.prathiba20@gmail.com>
2016-08-27 23:02:34 +0530

    Create Countprime.java

---
+++ Countprime.java
@@ -0,0 +1,20 @@
+    public class Prime
+    {
+    	private static boolean primeCounter(int number)
+    	{
+    		int counter = 0
+    		    		for (int i = 2; i < number; i++)
+    	        {
+    		if (number % i == 0)
+    			return false;
+    		else
+    			return true;
+    		if (false);
+    		counter++;
+    	        }
+    	}
+      public static void main(String[] args)
+    	{
+    		String userInput = JOptionPane.showInputDialog(null, "Please enter a positive Integer:");
+    		int num = Integer.parseInt(userInput);
+    }
